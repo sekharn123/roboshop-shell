13abeee3fd73576569d3f1fae81abbe58e689498
lenovo <venkatraja683@gmail.com>
2023-10-16 16:01:18 +0530

    sample

---
+++ 01.frontend.sh
@@ -0,0 +1,18 @@
+echo -e "\e[33m Installing nginx Server \e[0m"
+yum install nginx -y &>>/tmp/roboshop.log
+echo -e "\e[33m Removing default nginx content\e[0m"
+cd /usr/share/nginx/html
+rm -rf * &>>/tmp/roboshop.log
+useradd roboshop
+mkdir /app
+cd /app
+echo -e "\e[33m Download New Content to nginx \e[0m"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+unzip frontend.zip &>>/tmp/roboshop.log
+rm -rf frontend.zip
+echo -e "\e[33m Configuring reverse proxy server\e[0m"
+cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[33m Enabling and starting the nginx server\e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
+
+systemctl restart nginx
